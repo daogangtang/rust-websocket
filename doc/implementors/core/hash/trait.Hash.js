c0ab4cdf5dd2bccb6f7151448f112201e76ef050
cyderize <admin@cyderize.org>
2014-12-22 13:27:18 +1100

    Update for 0.8.2

---
+++ doc/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['websocket'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;S: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Writer.html' title='core::hash::Writer'>Writer</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&lt;S&gt; for Url",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
