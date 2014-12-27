c54ae3a312e4a8fd99e6cddf11de9488a229746b
cyderize <admin@cyderize.org>
2014-12-27 14:09:41 +1100

    Update for 0.9.0, include doc deps

---
+++ doc/implementors/std/io/trait.Acceptor.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['hyper'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Acceptor.html' title='std::io::Acceptor'>Acceptor</a>&lt;<a class='enum' href='hyper/net/enum.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>&gt; for <a class='struct' href='hyper/net/struct.HttpAcceptor.html' title='hyper::net::HttpAcceptor'>HttpAcceptor</a>",];
+implementors['websocket'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Acceptor.html' title='std::io::Acceptor'>Acceptor</a>&lt;<a class='enum' href='hyper/net/enum.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>&gt; for <a class='struct' href='hyper/net/struct.HttpAcceptor.html' title='hyper::net::HttpAcceptor'>HttpAcceptor</a>","<a class='stability Experimental' title='Experimental'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Acceptor.html' title='std::io::Acceptor'>Acceptor</a>&lt;<a class='struct' href='websocket/handshake/request/struct.WebSocketRequest.html' title='websocket::handshake::request::WebSocketRequest'>WebSocketRequest</a>&lt;<a class='enum' href='websocket/common/stream/enum.WebSocketStream.html' title='websocket::common::stream::WebSocketStream'>WebSocketStream</a>, <a class='enum' href='websocket/common/stream/enum.WebSocketStream.html' title='websocket::common::stream::WebSocketStream'>WebSocketStream</a>, <a class='struct' href='websocket/common/struct.Inbound.html' title='websocket::common::Inbound'>Inbound</a>&gt;&gt; for <a class='struct' href='websocket/server/struct.WebSocketAcceptor.html' title='websocket::server::WebSocketAcceptor'>WebSocketAcceptor</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
