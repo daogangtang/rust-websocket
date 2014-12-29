c69fde7184dd1856091b7e0046e85b30ed41b31c
cyderize <admin@cyderize.org>
2014-12-29 13:27:34 +1100

    Update for 0.9.2

--- doc/implementors/url/parser/trait.StrCharRanges.js
+++ doc/implementors/url/parser/trait.StrCharRanges.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['hyper'] = [];
+implementors['websocket'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/url/trait.ToUrlPath.js
+++ doc/implementors/url/trait.ToUrlPath.js
@@ -1,6 +1,7 @@
 (function() {var implementors = {};
 implementors['url'] = [];
 implementors['hyper'] = [];
+implementors['websocket'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
